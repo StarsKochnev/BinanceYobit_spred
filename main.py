7f7e21417701cdf5e47bc7d55a4f0f58fabd3626
StarsKochnev <kochnevstas@mail.ru>
2022-07-13 00:18:54 +0300

    убрал лишнее

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import time
-
 import json
 import requests
-
-import os
-
 #def parse(url):
     #api = requests.get(url)
     #tree = lxml.html.document_fromstring(api.text)
@@ -13,36 +9,28 @@ import os
     #print('Продажа Ltc- rub', text_original)
 
 #parse('https://yobit.biz/ru/trade/LTC/RUR#24H')
-
-
-
 # defining key/request url
 while True:
     key = "https://api.binance.com/api/v3/ticker/price?symbol=LTCRUB"
-
     # requesting data from url
     data = requests.get(key)
     data = data.json()
     z = data['price']
     last_trade_binance = round(float(z))
-    print(last_trade_binance)
+    print(f"{data['symbol']} Binance цена: {data['price']}")
 
-
-    print(f"{data['symbol']} price is {data['price']}")
     key2 = "https://yobit.net/api/3/ticker/ltc_rur"
     data = requests.get(key2)
     data = data.json()
     #print(data)
     b = data['ltc_rur']
-
     last_trade = int(round(b['last']))
-
-    print(f"Yobit {last_trade}")
-
+    print(f"LTCRUB Yobit цена: {last_trade}")
+    # считаем разницу
     percent = 100 / (last_trade - last_trade_binance) / last_trade_binance
     fg = str(percent)
     f = fg.replace('e-05', '')
-    y = round(float(f), 3)
-    print(y)
-    print('Разница между курсами валют в %', y)
-    time.sleep(2)
+    percent_fin = round(float(f), 3)
+
+    print('Разница между курсами валют в %', percent_fin)
+    time.sleep(5)
